d4e1f0714af4697a1a9261956e64633c0f7a3093
babariviere <babathriviere@gmail.com>
2019-06-09 14:16:04 +0200

    add storage definition

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,18 @@
 //! Library for boat program.
 
 pub mod otp;
+pub mod storage;
+
+/// Token used to generate OTP code
+pub struct Token {
+    // Unique identifier to retrieve token
+    id: String,
+    // Name of the token
+    name: String,
+    // Logo URL
+    logo: Option<String>,
+    // Secret to generate OTP code
+    secret: Vec<u8>,
+    // Length of the OTP code to generate
+    digit: u8,
+}
